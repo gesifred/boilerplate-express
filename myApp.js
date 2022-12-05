52a56d2683f2f067c13b1226278639d6ea093cf6
Wilfred V <gesifred@gmail.com>
2022-12-05 11:01:12 +0000

    static dir

--- myApp.js
+++ myApp.js
@@ -1,5 +1,8 @@
 let express = require('express');
+let path = require('path');
 let app = express();
+
+app.use("/public", express.static(path.join(__dirname, '/public')));
 
 app.get("/", (req, res) => {
   let pth = __dirname + "/views/index.html";
@@ -7,36 +10,5 @@ app.get("/", (req, res) => {
 });
 
 console.log("Hello World");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = app;
